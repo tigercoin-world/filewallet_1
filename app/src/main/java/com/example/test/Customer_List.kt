e7c8d31a706c2c13fd0125f95e8d29ce5a5b5550
tigercoin <khosla@naver.com>
2022-01-24 00:19:12 +0900

    1/24

--- app/src/main/java/com/example/test/Customer_List.kt
+++ app/src/main/java/com/example/test/Customer_List.kt
@@ -2,6 +2,7 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.test.databinding.ActivityCustomerListBinding
 
 
@@ -15,5 +16,9 @@ class CustomerList : AppCompatActivity() {
 
         mBinding = ActivityCustomerListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val item = arrayOf("사과", "배", "키위","바나나")
+        //context는 한 activity에 모든 정보를 담고 있는
+       binding.listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, item)
     }
 }
